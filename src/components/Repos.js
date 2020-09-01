72adc5cee21c4fe0bfbff84e1521e8db437ab0c6
jojowabbit <jojowabbit@gmail.com>
2020-09-01 14:50:16 +0800

    formatting stars & forks data

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -9,6 +9,7 @@ const Repos = () => {
     { label: "data3", value: 30 },
   ];
   const { repos } = useContext(GithubContext);
+
   const languages = repos.reduce((total, item) => {
     const { language, stargazers_count } = item;
     // language is null, return total
@@ -28,12 +29,16 @@ const Repos = () => {
     }
     return total;
   }, {});
+
+  // most used languages
   // take values from obj returns an array -> sort by highest to lowest -> slice top 5 out
   const mostUsed = Object.values(languages)
     .sort((a, b) => {
       return b.value - a.value;
     })
     .slice(0, 5);
+
+  // most popular languages
   // take values from obj returns an array -> sort by highest to lowest -> map stars to value as chart is accepting label+value only, ->slice top 5 out
   const mostPopular = Object.values(languages)
     .sort((a, b) => {
@@ -43,15 +48,32 @@ const Repos = () => {
       return { ...item, value: item.stars };
     })
     .slice(0, 5);
-  console.log(mostUsed);
+  // stars & forks
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item;
+      total.stars[stargazers_count] = { label: name, value: stargazers_count };
+      total.forks[forks] = { label: name, value: forks };
+      return total;
+    },
+    {
+      stars: {},
+      forks: {},
+    }
+  );
+
+  // get the last 5 value and reverse it
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
+
   return (
     <section className="section">
       <Wrapper className="section-center">
         <Pie3D data={mostUsed} />
 
-        <Column3D data={data} />
+        <Column3D data={stars} />
         <Doughnut2D data={mostPopular} />
-        <Bar3D data={data} />
+        <Bar3D data={forks} />
       </Wrapper>
     </section>
   );
