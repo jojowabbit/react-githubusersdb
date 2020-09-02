9beb6026dad2571568aff69242837473aa0f7d95
jojowabbit <jojowabbit@gmail.com>
2020-09-02 10:24:37 +0800

    Promise.allsettledd

--- src/context/context.js
+++ src/context/context.js
@@ -38,14 +38,30 @@ const GithubProvider = ({ children }) => {
     if (response) {
       setGithubUser(response.data);
       const { login, followers_url } = response.data;
-      // fetch repos
-      axios(`${rootUrl}/users/${login}/repos?per_page=100`).then((response) =>
-        setRepos(response.data)
-      );
-      // fetch followers
-      axios(`${followers_url}?per_page=100`).then((response) =>
-        setFollowers(response.data)
-      );
+      // // fetch repos
+      // axios(`${rootUrl}/users/${login}/repos?per_page=100`).then((response) =>
+      //   setRepos(response.data)
+      // );
+      // // fetch followers
+      // axios(`${followers_url}?per_page=100`).then((response) =>
+      //   setFollowers(response.data)
+      // );
+      // use promise.allsettled
+      await Promise.allSettled([
+        axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+        axios(`${followers_url}?per_page=100`),
+      ])
+        .then((results) => {
+          const [repos, followers] = results;
+          const status = "fulfilled";
+          if (repos.status === status) {
+            setRepos(repos.value.data);
+          }
+          if (followers.status === status) {
+            setFollowers(followers.value.data);
+          }
+        })
+        .catch((err) => console.log(err));
     } else {
       toggleError(false, "there is no user with that username");
     }
