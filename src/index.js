7e1470a5bed62faf8905eb7b91a2e842ffb7f833
jojowabbit <jojowabbit@gmail.com>
2020-09-02 12:23:23 +0800

    changes to autho0 variables & login, logout setup

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
-      domain={process.env.AUTH0_DOMAIN}
-      clientId={process.env.AUTH0_CID}
+      domain={process.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH0_CID}
       redirectUri={window.location.origin}
     >
       <GithubProvider>
